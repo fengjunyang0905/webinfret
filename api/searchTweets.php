585f0cecddc1d7ff53c630e454548b2f10b5f44b
Sander Breukink <s.breukink@outlook.com>
2017-01-04 16:19:56 +0100

    api update + maxheight

--- api/searchTweets.php
+++ api/searchTweets.php
@@ -16,9 +16,9 @@ results:
  */
 sleep(1);
 $return = array(
-    array("text" => "tweet 1 content", "rumor" => "No", "certaintity" => 0.67, "articles" => 1),
-    array("text" => "tweet 2 content", "rumor" => "Yes", "certaintity" => 0.89, "articles" => 2),
-    array("text" => "tweet 3 content, input data: " . $_GET["query"], "rumor" => "Unsure", "certaintity" => 0.12, "articles" => 1)
+    array("userid" => 1001, "text" => "tweet 1 content", "rumor" => "No", "certaintity" => 0.67, "articles" => array("http://www.washingtontimes.com/news/2016/oct/17/the-russian-enigma-and-the-us-election/")),
+    array("userid" => 1002, "text" => "tweet 2 content", "rumor" => "Yes", "certaintity" => 0.89, "articles" => array("http://www.washingtontimes.com/news/2016/oct/21/brian-babin-defends-donald-trump-a-lady-needs-to-b/","http://www.wsj.com/articles/denial-of-service-web-attack-affects-amazon-twitter-others-1477056080")),
+    array("userid" => 1003, "text" => "tweet 3 content, input data: " . $_GET["query"], "rumor" => "Unsure", "certaintity" => 0.12, "articles" => array())
 );
 
 //$return = array("still","left","to","do",$_GET["query"]);
