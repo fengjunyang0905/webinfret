a43b518fbd010f22c244a8bc07ea8537d98d39dd
Sander Breukink <s.breukink@outlook.com>
2017-01-14 15:08:27 +0100

    DO THIS IF ADRIAN FIXES HIS STUFF.

--- api/articlesearch.php
+++ api/articlesearch.php
@@ -1,5 +1,8 @@
 <?php
 //looks for articles
+
+//in case Adrian fixes his code, remove the current code and replace with: echo file_get_contents("group1.ga:8080/SampleApp/getArticles?query=" . $_GET['query'] . "&startingPoint=" . $_GET['startingPoint']);
+
 sleep(1);
 
 if($_GET["startingPoint"] > 10){
